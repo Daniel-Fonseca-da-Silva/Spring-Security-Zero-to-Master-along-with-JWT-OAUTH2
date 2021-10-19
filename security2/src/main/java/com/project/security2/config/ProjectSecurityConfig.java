dec5f2b114676f49241388f8e53ee5fcaf4b90ae
Daniel Fonseca da Silva <developer-web@programmer.net>
2021-10-18 22:11:38 -0300

    Created Custom requirements for endpoints

--- security2/src/main/java/com/project/security2/config/ProjectSecurityConfig.java
+++ security2/src/main/java/com/project/security2/config/ProjectSecurityConfig.java
@@ -1,15 +1,27 @@
 package com.project.security2.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
-		http.formLogin();
-		http.httpBasic();
-	}
+	//	This don't working with postman! only browser!
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+	        .antMatchers("/myAccount").authenticated()
+	        .antMatchers("/myBalance").authenticated()
+	        .antMatchers("/myLoans").authenticated()
+	        .antMatchers("/myCards").authenticated()
+	        .antMatchers("/notices").permitAll()
+	        .antMatchers("/contact").permitAll()
+        .and()
+        .formLogin()
+        .and()
+        .httpBasic();
+    }
 	
 }
