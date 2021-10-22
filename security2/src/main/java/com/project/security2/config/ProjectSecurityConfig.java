aabebe419cd68f756275b9354d749691e2a46be3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-22 01:09:32 -0300

    Configured users with inMemoryAuthentication

--- security2/src/main/java/com/project/security2/config/ProjectSecurityConfig.java
+++ security2/src/main/java/com/project/security2/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.project.security2.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,39 +14,24 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//	      .antMatchers("/myAccount").authenticated()
-//	      .antMatchers("/myBalance").authenticated()
-//	      .antMatchers("/myLoans").authenticated()
-//	      .antMatchers("/myCards").authenticated()
-//	      .antMatchers("/notices").permitAll()
-//	      .antMatchers("/contact").permitAll()
-//        .and()
-//        .formLogin()
-//        .and()
-//        .httpBasic();
-
-//    	// Configuration to deny all requests
-//    	http 
-//    	.authorizeRequests()
-//    	.anyRequest().
-//    	denyAll()
-//    	.and()
-//    	.formLogin()
-//    	.and() 
-//    	.httpBasic();
-    	
-    	
-    	// Configuration to permit all requests
-    	http 
-    	.authorizeRequests()
-    	.anyRequest().
-    	permitAll()
-    	.and()
-    	.formLogin()
-    	.and() 
-    	.httpBasic();
+        http.authorizeRequests()
+	      .antMatchers("/myAccount").authenticated()
+	      .antMatchers("/myBalance").authenticated()
+	      .antMatchers("/myLoans").authenticated()
+	      .antMatchers("/myCards").authenticated()
+	      .antMatchers("/notices").permitAll()
+	      .antMatchers("/contact").permitAll()
+        .and()
+        .formLogin()
+        .and()
+        .httpBasic();
     	
     }
+    
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin")
+		.and().withUser("user").password("123").authorities("read").and()
+		.passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
 	
 }
