3a728dda8f1a9b7ab075f97b7dc117e54a2b926e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-22 00:09:23 -0300

    Tested the denyAll

--- security2/src/main/java/com/project/security2/config/ProjectSecurityConfig.java
+++ security2/src/main/java/com/project/security2/config/ProjectSecurityConfig.java
@@ -11,17 +11,23 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-	        .antMatchers("/myAccount").authenticated()
-	        .antMatchers("/myBalance").authenticated()
-	        .antMatchers("/myLoans").authenticated()
-	        .antMatchers("/myCards").authenticated()
-	        .antMatchers("/notices").permitAll()
-	        .antMatchers("/contact").permitAll()
-        .and()
-        .formLogin()
-        .and()
-        .httpBasic();
+//        http.authorizeRequests()
+//	        .antMatchers("/myAccount").authenticated()
+//	        .antMatchers("/myBalance").authenticated()
+//	        .antMatchers("/myLoans").authenticated()
+//	        .antMatchers("/myCards").authenticated()
+//	        .antMatchers("/notices").permitAll()
+//	        .antMatchers("/contact").permitAll()
+//        .and()
+//        .formLogin()
+//        .and()
+//        .httpBasic();
+    	
+    	http .authorizeRequests()
+    	.anyRequest().denyAll()
+    	.and()
+    	.formLogin().and() .httpBasic();
+    	
     }
 	
 }
